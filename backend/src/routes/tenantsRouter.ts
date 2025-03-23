ee01702f9c9b55dbc3e4b00ba7a718fe0bb3be90
Andres Loreto <andresantigoloretoaguero@gmail.com>
2025-03-23 00:15:42 -0400

    remove tenants endpoints

--- backend/src/routes/tenantsRouter.ts
+++ backend/src/routes/tenantsRouter.ts
@@ -4,13 +4,6 @@ import tenantRouter from "./tenantRouter";
 
 const tenantsRouter = express.Router();
 
-tenantsRouter.get("/", (req, res) => {
-  res.send("Return list of tenants. Work in progress");
-});
-tenantsRouter.post("/", (req, res) => {
-  res.send("Create a new tenant. Work in progress");
-});
-
 tenantsRouter.use("/:tenantId", tenantRouter);
 
 export default tenantsRouter;
