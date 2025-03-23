a657d1db8e5a6dac608d8e3955135e85217098a4
Andres Loreto <andresantigoloretoaguero@gmail.com>
2025-03-22 20:55:46 -0400

    Make getAuth0Config configurable

--- backend/src/config/auth0.ts
+++ backend/src/config/auth0.ts
@@ -1,14 +1,13 @@
-export const getAuth0Config = ({
-  secret,
-  baseURL,
-}: {
-  secret: string;
-  baseURL: string;
-}) => ({
-  authRequired: false,
-  auth0Logout: true,
-  secret: secret,
-  baseURL: baseURL,
-  clientID: "6Ryf7Mr5YBHtI37oOJozKoWAbvZP7koT",
-  issuerBaseURL: "https://dev-qe6kdzk4qf8rxwsx.us.auth0.com",
+import type { ConfigParams } from "express-openid-connect";
+
+export const getAuth0Config = (
+  config: ConfigParams & Required<Pick<ConfigParams, "secret" | "baseURL">>,
+): ConfigParams => ({
+  authRequired: config.authRequired ?? false,
+  auth0Logout: config.auth0Logout ?? true,
+  secret: config.secret,
+  baseURL: config.baseURL,
+  clientID: config.clientID ?? "6Ryf7Mr5YBHtI37oOJozKoWAbvZP7koT",
+  issuerBaseURL:
+    config.issuerBaseURL ?? "https://dev-qe6kdzk4qf8rxwsx.us.auth0.com",
 });
