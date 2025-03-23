a82949f85289a9f290ea2d70da3a591728e04f18
Andres Loreto <andresantigoloretoaguero@gmail.com>
2025-03-22 23:36:56 -0400

    Allow user model to have multiple departments

---
+++ backend/prisma/migrations/20250321032808_turned_department_and_user_relationship_many_to_many/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `departmentId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "User" DROP CONSTRAINT "User_departmentId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "departmentId";
+
+-- CreateTable
+CREATE TABLE "_DepartmentToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_DepartmentToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_DepartmentToUser_B_index" ON "_DepartmentToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_DepartmentToUser" ADD CONSTRAINT "_DepartmentToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Department"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_DepartmentToUser" ADD CONSTRAINT "_DepartmentToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
