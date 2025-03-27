d0463b869d42111fd3f7e1eb0acb4aff962d6220
Andres Loreto <andresantigoloretoaguero@gmail.com>
2025-03-26 23:09:59 -0400

    Add tenant service for accesing prisma

---
+++ backend/src/services/tenant.ts
@@ -0,0 +1,29 @@
+import prismaClient from "@/prismaClient";
+import type { Tenant } from "@prisma/client";
+import TenantNotFoundError from "@/errors/TenantNotFoundError";
+
+/**
+ * Returns a tenant by id.
+ */
+export const findTenantById = async (id: number): Promise<Tenant | null> => {
+  const tenant = await prismaClient.tenant.findUnique({
+    where: { id },
+  });
+
+  return tenant;
+};
+
+/**
+ * Returns a tenant by id or throws an error if not found.
+ */
+export const findTenantByIdStrict = async (id: number): Promise<Tenant> => {
+  const tenant = await findTenantById(id);
+
+  if (!tenant) {
+    throw new TenantNotFoundError({ id });
+  }
+
+  return tenant;
+};
+
+export type { Tenant };
